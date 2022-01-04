866bdbbcfa73c36c8286e5a5ba229b6826ec45dd
sasha <sasha432301@gmail.com>
2022-01-04 20:05:09 +0300

    FunctionsV1

--- js/main.js
+++ js/main.js
@@ -7,10 +7,35 @@ function getUrlByCity(city, date = "") {
   }
   return url;
 }
-function dayWeatherPreview() {
-  for (let i = 0; i < 7; i++) {
+function dayWeatherPreview(data) {
+  console.log(data);
+  let mainDiv = document.createElement("div");
+  mainDiv.className = "dayWeatherPreview__main";
+  for (let i = 0; i < 3; i++) {
     let div = document.createElement("div");
-    let datee = data.forecast.forecastday[i].date;
-    let icon = data.forecast.forecastday[i].day.condition.icon;
+    div.className = "dayWeatherPreview__block";
+    let obj = data.forecast.forecastday[i];
+    let datee = obj.date;
+    let img = document.createElement("img");
+    let icon = obj.day.condition.icon;
+    img.src = icon;
+    let temp = `${obj.day.avgtemp_c}°`;
+    let about = obj.day.condition.text;
+    div.append(datee);
+    div.append(img);
+    div.append(temp);
+    div.append(about);
+    mainDiv.append(div);
   }
+  return mainDiv;
 }
+
+fetch(
+  "http://api.weatherapi.com/v1/forecast.json?key=b42ec53b7614492ea26173900212112&q=London&days=7&aqi=yes&alerts=yes"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    let block = dayWeatherPreview(data);
+    document.body.append(block);
+  });
