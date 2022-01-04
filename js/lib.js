62bb0c128a836fb565ce9a578cf277dcb6373c9d
Yana Kochurko <yanaivanovva04@gmail.com>
2022-01-04 19:32:08 +0300

    header

---
+++ js/lib.js
@@ -0,0 +1,13 @@
+// For the time now
+Date.prototype.timeNow = function () {
+  return (
+    (this.getHours() < 10 ? "0" : "") +
+    this.getHours() +
+    ":" +
+    (this.getMinutes() < 10 ? "0" : "") +
+    this.getMinutes() +
+    ":" +
+    (this.getSeconds() < 10 ? "0" : "") +
+    this.getSeconds()
+  );
+};
